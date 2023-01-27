1df33923cd4c4d0e5688c2378112f2a7e379135f
Sumeet Kumar <64634052+sumeetkvd@users.noreply.github.com>
2023-01-27 17:38:42 +0530

    Create Watchdog-Timer-RISC-V.v
    This module has five inputs: clk, rst, en, timeout, and an output wdt. The clk input is the clock signal that drives the timer, rst is the reset signal, en is the enable signal, and timeout is the value that the counter cnt will be compared against, to decide if the watchdog timer has expired. The wdt output is a signal that indicates when the timer has expired.
    
    The module uses an internal 32-bit counter cnt that is incremented on each rising edge of the clk signal, as long as the en signal is high. The counter is reset to 0 when the rst signal is high.
    
    When the counter reaches its maximum value timeout, the wdt signal is set to 1 and the counter is reset to 0. Otherwise, the wdt signal is set to 0.
    
    And here's an example of a testbench for the above watchdog timer:

---
+++ Watchdog-Timer-RISC-V.v
@@ -0,0 +1,18 @@
+module watchdog_timer (input clk, input rst, input en, input [4:0] timeout, output reg wdt);
+   reg [31:0] cnt;
+   always @(posedge clk) begin
+      if (rst) begin
+         cnt <= 32'h0;
+      end else if (en) begin
+         cnt <= cnt + 1;
+      end
+   end
+   always @(posedge clk) begin
+      if (cnt == timeout) begin
+         wdt <= 1;
+         cnt <= 32'h0;
+      end else begin
+         wdt <= 0;
+      end
+   end
+endmodule
